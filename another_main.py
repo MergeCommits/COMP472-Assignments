b25481faeaa4be0457c906fbbae5cf40e7a0ccf7
Mark <markjarjour@gmail.com>
2021-02-21 15:55:50 -0500

    Added Ghislain.

--- another_main.py
+++ another_main.py
@@ -2,13 +2,14 @@ from __future__ import division
 
 from codecs import open
 
-import sklearn
-from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.model_selection import GridSearchCV
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier
+
+from naive_bayes import naive_bayes
+
+SPLIT_PERCENT = 0.8
+DOCUMENT_NAME = "all_sentiment_shuffled.txt"
 
 
 def read_documents(doc_file):
@@ -22,79 +23,58 @@ def read_documents(doc_file):
     return docs, labels
 
 
-def train_model(documents: list, labels: list, pipeline: sklearn.pipeline.Pipeline) -> Pipeline:
-    return pipeline.fit(documents, labels)
+def get_labels(labels):
+    label_list = []
+    for label in labels:
+        if label not in label_list:
+            label_list.append(label)
+
+    return label_list
 
 
-def predict_eval_data(documents: list, labels: list, model: sklearn.pipeline.Pipeline):
-    predicted_labels = model.predict(documents)
+def main():
+    all_docs, all_labels = read_documents(DOCUMENT_NAME)
+    split_point = int(SPLIT_PERCENT * len(all_docs))
 
-    target_labels = [labels[0], "pos" if labels[0] == "neg" else "neg"]
+    train_docs = all_docs[:split_point]
+    train_labels = all_labels[:split_point]
 
-    print(metrics.classification_report(labels, predicted_labels, labels=target_labels))
-    print(metrics.confusion_matrix(labels, predicted_labels))
+    eval_docs = all_docs[split_point:]
+    eval_labels = all_labels[split_point:]
+
+    all_labels = get_labels(train_labels)
+
+    base_dt_pipeline = Pipeline([
+        ('vec', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', DecisionTreeClassifier(criterion="entropy", splitter="random")),
+    ])
+
+    best_dt_pipeline = Pipeline([
+        ('vec', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', DecisionTreeClassifier(criterion="entropy", splitter="best"))
+    ])
+
+    naive_bayes(train_docs,
+                train_labels,
+                eval_docs,
+                eval_labels,
+                all_labels)
+
+    # run_train_and_analyze(base_dt_pipeline,
+    #                       train_docs,
+    #                       train_labels,
+    #                       eval_docs,
+    #                       eval_labels,
+    #                       "basedt-reviews.txt")
+    #
+    # run_train_and_analyze(best_dt_pipeline,
+    #                       train_docs,
+    #                       train_labels,
+    #                       eval_docs,
+    #                       eval_labels,
+    #                       "bestdt-reviews.txt")
 
 
-def run_train_and_analyze(pipeline: sklearn.pipeline.Pipeline,
-                          x_train: list,
-                          y_train: list,
-                          x_test: list,
-                          y_truth: list,
-                          filename: str):
-    with open(filename, 'w') as f:
-        import sys
-        # sys.stdout = f  # Change the standard output to the file we created.
-        ml_model = train_model(x_train, y_train, pipeline)
-        predict_eval_data(x_test, y_truth, ml_model)
-
-
-naive_bayes_pipeline = Pipeline([
-    ('vec', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', MultinomialNB(alpha=0.0)),
-])
-
-base_dt_pipeline = Pipeline([
-    ('vec', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', DecisionTreeClassifier(criterion="entropy", splitter="random")),
-])
-
-tree_para = {'criterion': ['gini', 'entropy'],
-             'max_depth': [4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 30, 40, 50, 70, 90, 120, 150]}
-
-clf = GridSearchCV(DecisionTreeClassifier(), tree_para, cv=5)
-best_dt_pipeline = Pipeline([
-    ('vec', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', DecisionTreeClassifier(criterion="entropy", splitter="best"))
-    # ('clf', GridSearchCV(DecisionTreeClassifier, tree_para, cv=5))
-])
-
-all_docs, all_labels = read_documents("all_sentiment_shuffled.txt")
-split_point = int(0.80 * len(all_docs))
-train_docs = all_docs[:split_point]
-train_labels = all_labels[:split_point]
-eval_docs = all_docs[split_point:]
-eval_labels = all_labels[split_point:]
-
-run_train_and_analyze(naive_bayes_pipeline,
-                      train_docs,
-                      train_labels,
-                      eval_docs,
-                      eval_labels,
-                      "nb-reviews.txt")
-
-run_train_and_analyze(base_dt_pipeline,
-                      train_docs,
-                      train_labels,
-                      eval_docs,
-                      eval_labels,
-                      "basedt-reviews.txt")
-
-run_train_and_analyze(best_dt_pipeline,
-                      train_docs,
-                      train_labels,
-                      eval_docs,
-                      eval_labels,
-                      "bestdt-reviews.txt")
+main()
---
+++ naive_bayes.py
@@ -0,0 +1,36 @@
+from sklearn import metrics
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from train_and_analyze import train_model, predict_eval_data, return_some_wrong_predictions, print_to_file
+
+NAIVES_BAYES_SMOOTHING = float(1)
+
+
+def naive_bayes(x_train: list,
+                y_train: list,
+                x_test: list,
+                y_truth: list,
+                all_labels: list):
+    naive_bayes_pipeline = Pipeline([
+        ('vec', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultinomialNB(alpha=NAIVES_BAYES_SMOOTHING)),
+    ])
+
+    with open("nb-reviews.txt", 'w') as f:
+        import sys
+        # sys.stdout = f  # Change the standard output to the file we created.
+        ml_model = train_model(x_train, y_train, naive_bayes_pipeline)
+        predicted_labels = predict_eval_data(x_test, ml_model)
+
+        report = metrics.classification_report(y_truth, predicted_labels, labels=all_labels)
+        confusion_matrix = metrics.confusion_matrix(y_truth, predicted_labels)
+        wrong_predictions = return_some_wrong_predictions(all_labels, y_train, x_test, y_truth, predicted_labels)
+
+        row_predictions = ""
+        for i in range(len(predicted_labels)):
+            row_predictions += f'Row #{i + 1} predicts {predicted_labels[i]}\n'
+
+        print_to_file("Naives Bayes Classifier", report, confusion_matrix, wrong_predictions, row_predictions)
