d6f994e6410dc4de7e9368022c6ce01f0464c96a
Mark <markjarjour@gmail.com>
2021-03-30 19:54:18 -0400

    20 runs

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import time
 from typing import Tuple, List, Optional
 
 from structures.Puzzle import Puzzle
@@ -45,3 +46,42 @@ def write_to_file(puzzle: Puzzle, algo_function, filename: str):
         for element in visit_path:
             element.smart_print()
             print()
+
+
+def analyze_20_runs(puzzles: List[Puzzle], algo_function):
+    total_runs = len(puzzles)
+    total_goal_path_length = 0
+    total_search_path_length = 0
+    total_no_solutions = 0
+    total_time = float(0)
+
+    for puzzle in puzzles:
+        start_time = time.time()
+        result = perform_run(puzzle, algo_function)
+        if result is None:
+            total_no_solutions += 1
+            total_time += 60
+        else:
+            end_time = time.time()
+
+            total_goal_path_length += len(result[0])
+            total_search_path_length += len(result[1])
+
+            elapsed_time = end_time - start_time
+            total_time += elapsed_time
+
+    avg_goal_path_length = total_goal_path_length / total_runs
+    avg_search_path_length = total_search_path_length / total_runs
+    avg_time = total_time / total_runs
+
+    print("Average goal path length: " + str(avg_goal_path_length))
+    print("Total goal path length: " + str(total_goal_path_length))
+
+    print("Average search path length: " + str(avg_search_path_length))
+    print("Total search path length: " + str(total_search_path_length))
+
+    print("Failed runs: " + str(total_no_solutions) + "/" + str(total_runs))
+
+    print("Average runtime: " + str(avg_time))
+    print("Total runtime: " + str(total_time))
+
--- another_main.py
+++ another_main.py
@@ -1,9 +1,13 @@
 import multiprocessing
+import random
+from typing import List
+
+import numpy
 
 from algos.AStar import atar_incorrect_position_heuristic, atar_distance_to_incorrect
 from algos.DFS import dfs_search
 from algos.ID import id_search
-from analysis.Analysis import write_to_file
+from analysis.Analysis import write_to_file, analyze_20_runs
 from structures.Puzzle import Puzzle
 
 
@@ -11,35 +15,80 @@ def run_search(puzzle: Puzzle, algo):
     algo(puzzle)
 
 
-def main():
-    # puz = Puzzle('((4; 3); (2; 1))')
-    puz = Puzzle('((6; 1; 2); (7; 8; 3); (5; 4; 9))')
-    # puz = Puzzle('((1; 2; 3; 4); (5; 6; 7; 8); (9; 10; 11; 12); (13; 14; 15; 16))')
-    # puz_list = puz.get_permutations()
-    # for puzzle in puz_list:
-    #     # print(puzzle)
-    #     puzzle.smart_print()
-    #     print()
-
-    # puz2 = Puzzle('((1; 2; 3); (4; 5; 6); (7; 8; 9))')
-    # print(puz.equals(puz2))
-
-    # print(Puzzle('((1; 2; 3; 4); (5; 6; 7; 8); (9; 10; 11; 12); (13; 14; 15; 16))').is_solved())
-
+def standard_run():
     if __name__ == '__main__':
+        # puz = Puzzle('((6; 1; 2); (7; 8; 3); (5; 4; 9))')
+        puz = Puzzle('((9; 7; 1); (5; 6; 2); (4; 8; 3))')
+
         p1 = multiprocessing.Process(target=write_to_file, name="dfs", args=(puz, dfs_search, "dfs.txt"))
         p1.start()
         p2 = multiprocessing.Process(target=write_to_file, name="id", args=(puz, id_search, "id.txt"))
         p2.start()
-        p3 = multiprocessing.Process(target=write_to_file, name="astar1", args=(puz, atar_incorrect_position_heuristic, "astar1.txt"))
+        p3 = multiprocessing.Process(target=write_to_file, name="astar1",
+                                     args=(puz, atar_incorrect_position_heuristic, "astar1.txt"))
         p3.start()
-        p4 = multiprocessing.Process(target=write_to_file, name="astar2", args=(puz, atar_distance_to_incorrect, "astar2.txt"))
+        p4 = multiprocessing.Process(target=write_to_file, name="astar2",
+                                     args=(puz, atar_distance_to_incorrect, "astar2.txt"))
         p4.start()
 
         p1.join()
         p2.join()
         p3.join()
         p4.join()
+
+
+def read_puzzles(filename) -> List[Puzzle]:
+    puzzles: List[Puzzle] = []
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            puzzles.append(Puzzle(line[:-1]))
+
+    return puzzles
+
+
+def twenty_run():
+    if __name__ == '__main__':
+        puzzles = read_puzzles("20puzzles.txt")
+        analyze_20_runs(puzzles, atar_incorrect_position_heuristic)
+
+        # p1 = multiprocessing.Process(target=write_to_file, name="dfs", args=(puz, dfs_search, "dfs.txt"))
+        # p1.start()
+        # p2 = multiprocessing.Process(target=write_to_file, name="id", args=(puz, id_search, "id.txt"))
+        # p2.start()
+        # p3 = multiprocessing.Process(target=write_to_file, name="astar1",
+        #                              args=(puz, atar_incorrect_position_heuristic, "astar1.txt"))
+        # p3.start()
+        # p4 = multiprocessing.Process(target=write_to_file, name="astar2",
+        #                              args=(puz, atar_distance_to_incorrect, "astar2.txt"))
+        # p4.start()
+        #
+        # p1.join()
+        # p2.join()
+        # p3.join()
+        # p4.join()
+
+
+def twenty_generate():
+    with open("20puzzles.txt", 'w') as f:
+        import sys
+        sys.stdout = f  # Change the standard output to the file we created.
+
+        size = 3
+
+        for i in range(20):
+            random_list = list(range(1, (size ** 2) + 1))
+            random.shuffle(random_list)
+            matrix = numpy.array(random_list).reshape([size, size])
+            puzzle = Puzzle('((6; 1; 2); (7; 8; 3); (5; 4; 9))')
+            puzzle.matrix = matrix
+            puzzle.dim = size
+            print(puzzle)
+
+
+def main():
+    standard_run()
+    # twenty_run()
+    # twenty_generate()
 
 
 main()
