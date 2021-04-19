6f66ebdc3b3c38908274b644a1f740320822f084
Mark <markjarjour@gmail.com>
2021-04-19 15:27:55 -0400

    snake_case

--- main.py
+++ main.py
@@ -3,34 +3,34 @@ import math
 
 
 class State:
-    nodesVisited = 0
-    nodesEvaluated = 0
-    maxDepth = 0
-    subtreeCount = 0
-    branchCount = 0
+    nodes_visited = 0
+    nodes_evaluated = 0
+    max_depth = 0
+    subtree_count = 0
+    branch_count = 0
 
-    def __init__(self, availableTokens, lastToken, currentPlayer, depth=-1):
-        self.availableTokens = availableTokens
-        self.lastToken = lastToken
+    def __init__(self, available_tokens, last_token, current_player, depth=-1):
+        self.available_tokens = available_tokens
+        self.last_token = last_token
         self.actions = None
-        self.currentPlayer = currentPlayer
+        self.current_player = current_player
         self.depth = depth
 
-        self.generateActions()
+        self.generate_actions()
 
-        State.nodesVisited += 1
+        State.nodes_visited += 1
 
-    def generateActions(self):
-        if self.availableTokens is None:
+    def generate_actions(self):
+        if self.available_tokens is None:
             return
 
         self.actions = []
-        for token in self.availableTokens:
-            if max(token, self.lastToken) % min(token, self.lastToken) == 0:
+        for token in self.available_tokens:
+            if max(token, self.last_token) % min(token, self.last_token) == 0:
                 self.actions.append(token)
 
     def __str__(self):
-        return f"Player {self.currentPlayer}'s turn: [{self.lastToken}] => {self.availableTokens}"
+        return f"Player {self.current_player}'s turn: [{self.last_token}] => {self.available_tokens}"
 
     def heuristic(self):
         heuristic = float(0)
@@ -44,14 +44,14 @@ class State:
             heuristic = 0
 
         # Case: 2 is the last picked token.
-        elif self.lastToken == 1:
-            if len(self.availableTokens) % 2 == 1:
+        elif self.last_token == 1:
+            if len(self.available_tokens) % 2 == 1:
                 heuristic = 0.5
             else:
                 heuristic = -0.5
 
         # Case: 3 is not available, and last picked token is a prime.
-        elif sympy.isprime(self.lastToken):
+        elif sympy.isprime(self.last_token):
             if len(self.actions) % 2 == 1:
                 heuristic = 0.7
             else:
@@ -59,12 +59,12 @@ class State:
 
         # Case: 4 is not available, and last picked token is a composite.
         else:
-            largestPrime = 0
+            largest_prime = 0
             for token in self.actions:
-                if self.lastToken % token == 0 and sympy.isprime(token) and token > largestPrime:
-                    largestPrime = token
+                if self.last_token % token == 0 and sympy.isprime(token) and token > largest_prime:
+                    largest_prime = token
 
-            if largestPrime == 0:
+            if largest_prime == 0:
                 if len(self.actions) % 2 == 1:
                     heuristic = 0.7
                 else:
@@ -72,8 +72,8 @@ class State:
 
             else:
                 multiples = 0
-                for token in self.availableTokens:
-                    if token % largestPrime == 0:
+                for token in self.available_tokens:
+                    if token % largest_prime == 0:
                         multiples += 1
 
                 if multiples % 2 == 1:
@@ -82,128 +82,128 @@ class State:
                     heuristic = -0.6
 
         # Flip if it's Min's turn.
-        if self.currentPlayer == "Min":
+        if self.current_player == "Min":
             heuristic *= -1
 
-        State.nodesEvaluated += 1
+        State.nodes_evaluated += 1
 
         return heuristic
 
-    def analyzeInput(self, inputState):
-        parameters = inputState.split()
+    def analyze_input(self, input_state):
+        parameters = input_state.split()
 
-        tokenCount = int(parameters[0])
-        self.availableTokens = list(range(1, tokenCount + 1))
+        token_count = int(parameters[0])
+        self.available_tokens = list(range(1, token_count + 1))
 
-        turnNum = int(parameters[1])
-        if turnNum % 2 == 0:
-            self.currentPlayer = "Max"
+        turn_num = int(parameters[1])
+        if turn_num % 2 == 0:
+            self.current_player = "Max"
         else:
-            self.currentPlayer = "Min"
+            self.current_player = "Min"
 
         # If the input is not a fresh game.
-        if turnNum > 0:
-            for i in parameters[2:2 + turnNum]:
-                self.availableTokens.remove(int(i))
+        if turn_num > 0:
+            for i in parameters[2:2 + turn_num]:
+                self.available_tokens.remove(int(i))
 
-            self.lastToken = int(parameters[-2])
+            self.last_token = int(parameters[-2])
 
-            self.generateActions()
+            self.generate_actions()
 
         # If the input is a fresh game.
         else:
             self.actions = []
-            for i in range(1, math.ceil(tokenCount / 2), 2):
+            for i in range(1, math.ceil(token_count / 2), 2):
                 self.actions.append(i)
 
         self.depth = int(parameters[-1])
         if self.depth == 0:
             self.depth = -1
 
-    def generateChild(self, action):
-        newTokens = self.availableTokens.copy()
-        newTokens.remove(action)
+    def generate_child(self, action):
+        new_tokens = self.available_tokens.copy()
+        new_tokens.remove(action)
 
-        return State(newTokens, action, self.getOppositePlayer(), self.depth - 1)
+        return State(new_tokens, action, self.get_opposite_player(), self.depth - 1)
 
-    def getOppositePlayer(self):
-        if self.currentPlayer == "Min":
+    def get_opposite_player(self):
+        if self.current_player == "Min":
             return "Max"
         else:
             return "Min"
 
     @staticmethod
-    def updateBranchingFactor(count):
-        State.subtreeCount += 1
-        State.branchCount += count
+    def update_branching_factor(count):
+        State.subtree_count += 1
+        State.branch_count += count
 
 
 def main():
     print(
         "Input the state of the game: <amount of tokens> <turns elapsed> <tokens taken in order> <max depth evaluation>")
-    stateInput = input()
+    state_input = input()
 
-    startNode = State(None, 0, "")
-    startNode.analyzeInput(stateInput)
-    result = alphaBetaPruning(startNode, -math.inf, math.inf)
+    start_node = State(None, 0, "")
+    start_node.analyze_input(state_input)
+    result = alpha_beta_pruning(start_node, -math.inf, math.inf)
 
     print(f"Move: {result[1]}")
     print(f"Value: {result[0]}")
-    print(f"Number of Nodes Visited: {State.nodesVisited}")
-    print(f"Number of Nodes Evaluated: {State.nodesEvaluated}")
-    print(f"Max Depth Reached: {State.maxDepth}")
-    print(f"Avg Effective Branching Factor: {State.branchCount / State.subtreeCount}")
+    print(f"Number of Nodes Visited: {State.nodes_visited}")
+    print(f"Number of Nodes Evaluated: {State.nodes_evaluated}")
+    print(f"Max Depth Reached: {State.max_depth}")
+    print(f"Avg Effective Branching Factor: {State.branch_count / State.subtree_count}")
 
 
-def alphaBetaPruning(state, alpha, beta, depth=0):
-    if (State.maxDepth < depth):
-        State.maxDepth = depth
+def alpha_beta_pruning(state, alpha, beta, depth=0):
+    if State.max_depth < depth:
+        State.max_depth = depth
 
     print(f"{'    ' * depth}{state}")
     if len(state.actions) == 0 or state.depth == 0:
         return [state.heuristic(), None]
 
-    bestScore = None
-    bestAction = None
-    totalActionsExamined = 0
+    best_score = None
+    best_action = None
+    total_actions_examined = 0
 
     for action in state.actions:
-        totalActionsExamined += 1
+        total_actions_examined += 1
         changed = False
-        childState = state.generateChild(action)
-        score = alphaBetaPruning(childState, alpha, beta, depth + 1)[0]
+        child_state = state.generate_child(action)
+        score = alpha_beta_pruning(child_state, alpha, beta, depth + 1)[0]
         print(f"{'    ' * (depth + 1)}{score}")
-        if bestScore is None:
-            bestScore = score
-            bestAction = action
+        if best_score is None:
+            best_score = score
+            best_action = action
             changed = True
 
         else:
-            if state.currentPlayer == "Max":
-                if score > bestScore:
-                    bestScore = score
-                    bestAction = action
+            if state.current_player == "Max":
+                if score > best_score:
+                    best_score = score
+                    best_action = action
                     changed = True
             else:
-                if score < bestScore:
-                    bestScore = score
-                    bestAction = action
+                if score < best_score:
+                    best_score = score
+                    best_action = action
                     changed = True
 
         if changed:
-            if state.currentPlayer == "Max":
-                if bestScore > alpha:
-                    alpha = bestScore
+            if state.current_player == "Max":
+                if best_score > alpha:
+                    alpha = best_score
             else:
-                if bestScore < beta:
-                    beta = bestScore
+                if best_score < beta:
+                    beta = best_score
 
             if alpha >= beta:
-                State.updateBranchingFactor(totalActionsExamined)
-                return [bestScore, bestAction]
+                State.update_branching_factor(total_actions_examined)
+                return [best_score, best_action]
 
-    State.updateBranchingFactor(totalActionsExamined)
-    return [bestScore, bestAction]
+    State.update_branching_factor(total_actions_examined)
+    return [best_score, best_action]
 
 
 main()
